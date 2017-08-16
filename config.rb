59031369199f720d2530060f7f736b93a6061b99
dmiotti <david@muxumuxu.com>
2017-08-16 15:28:15 +0200

    [+] Put the real content

--- config.rb
+++ config.rb
@@ -7,16 +7,10 @@ end
 
 activate :i18n, mount_at_root: :en, langs: [:fr, :en]
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -33,32 +27,3 @@ configure :build do
   activate :directory_indexes
   set :relative_links, true
 end
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
---
+++ source/javascripts/all.js
@@ -0,0 +1,4 @@
+setTimeout(function() { 
+  var firstFrame = document.getElementById('frame')
+  firstFrame.style.opacity = 0;
+}, 4000);
